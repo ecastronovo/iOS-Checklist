ac1ccb78b6c3d7f17252295836de606c00bc6a8c
eric <ecastronovo@scu.edu>
2019-09-08 21:33:26 -0700

    Initial Commit

---
+++ Checklist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Checklist
+//
+//  Created by Eric Castronovo on 9/8/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
